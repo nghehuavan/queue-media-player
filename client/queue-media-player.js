97ecba1a320baef131c6e45774324bc8fee05956
nghe.huavan <nghe.huavan@gmail.com>
2022-07-28 01:01:58 +0700

    modified:   client/queue-media-player.html

--- client/queue-media-player.js
+++ client/queue-media-player.js
@@ -13,11 +13,13 @@ class QueueMediaPlayer {
     this.networkWaiting = false;
 
     this.fetching = false;
-    this.fecthRetryInterval = null;
+    this.fetchingRetryInterval = null;
+    this.waitFecthingInterval = null;
 
     this.timeRanges = [];
-    this.totalDuration = 0;
     this.lastOffset = 0;
+
+    this.totalBufferDuration = 0;
 
     this.video = document.querySelector(videoSelector);
     this.mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
@@ -42,10 +44,10 @@ class QueueMediaPlayer {
     });
   };
 
-  addQueue = (arr) => {
+  addQueue = async (arr) => {
     this.queue.push(...arr);
     if (this.queueWaiting) {
-      this.queueShiftFecthAppendBuffer();
+      await this.queueShiftFecthAppendBuffer();
     }
   };
 
@@ -55,7 +57,7 @@ class QueueMediaPlayer {
 
     if (this.queue.length > 0) {
       // append first video for start
-      this.queueShiftFecthAppendBuffer({ isFirst: true });
+      await this.queueShiftFecthAppendBuffer({ isFirst: true });
       // play video;
       this.video.play();
     }
@@ -67,55 +69,61 @@ class QueueMediaPlayer {
       if (this.fetching) return;
       this.fetching = true;
 
-      const remaining = Math.abs(this.totalDuration - this.video.currentTime);
+      const remaining = Math.abs(this.totalBufferDuration - this.video.currentTime);
       if (remaining <= this.config.nextOnRemaining) {
         await this.queueShiftFecthAppendBuffer();
       }
       this.fetching = false;
     };
 
-    this.video.onplaying = (e) => {
-      console.log('this.video.onplaying at ' + this.video.currentTime + ' / ' + this.totalDuration);
-      this.queueWaiting = false;
-    };
-
-    // wating for next queue or some network slow
     this.video.onwaiting = async (e) => {
-      if (this.video.buffered.length == 0) return;
-      console.log('this.video.onwaiting at ' + this.video.currentTime + ' / ' + this.totalDuration);
-      console.log('this.queueWaiting, this.networkWaiting', this.queueWaiting, this.networkWaiting);
-      if (!this.queueWaiting && !this.networkWaiting) {
-        const index = this.timeRanges.findIndex((i) => i.from <= this.video.currentTime && i.to >= this.video.currentTime);
-        if (index >= 0 && index < this.timeRanges.length) {
-          this.seekBackDVR(index);
-        }
+      console.log('this.video.onwaiting at ' + this.video.currentTime + ' / ' + this.totalBufferDuration);
+      const index = this.timeRanges.findIndex((i) => i.from <= this.video.currentTime && i.to >= this.video.currentTime);
+      if (index >= 0) {
+        this.seekBackTimeRanges(index);
       }
     };
 
     this.video.onseeked = async (e) => {
       console.log(e);
-      console.log('this.video.onseeked at ' + this.video.currentTime + ' / ' + this.totalDuration);
-      console.log(this.video.readyState);
+      console.log('this.video.onseeked at ' + this.video.currentTime + ' / ' + this.totalBufferDuration);
+      const index = this.timeRanges.findIndex((i) => i.from <= this.video.currentTime && i.to >= this.video.currentTime);
+      if (index >= 0) {
+        this.seekBackTimeRanges(index);
+      }
     };
   };
 
-  seekBackDVR = async (index) => {
-    console.log('seekBack', index);
-    console.log(this.timeRanges);
-    const timeRange = this.timeRanges[index];
-    console.log(timeRange);
+  seekBackTimeRanges = async (index) => {
+    this.video.pause();
+    await this.waitForPendingFetching();
 
-    // if (needReload()) {
-    console.log('reload timeRange: ', timeRange);
-    const videoFecth = await this.fecthVideo(timeRange.url);
-    this.sourceBuffer.timestampOffset = timeRange.offset;
-    this.sourceBuffer.appendBuffer(videoFecth.buffer);
-    // }
+    // Rebuild queue from index
+    const reBuildQueue = this.timeRanges.filter((_, idx) => idx >= index).map((i) => i.url);
+    this.queue.unshift(...reBuildQueue);
+
+    // re-calulate totalBufferDuration by index
+    const reBufferDuration = this.timeRanges
+      .filter((_, idx) => idx < index)
+      .map((i) => i.duration)
+      .reduce((prev, curr) => prev + curr, 0);
+    this.totalBufferDuration = reBufferDuration;
+
+    await this.queueShiftFecthAppendBuffer({ isFirst: index == 0 });
+    this.video.play();
   };
 
-  needReload = () => {
-    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
-    return this.video.readyState < 4;
+  waitForPendingFetching = () => {
+    return new Promise(async (resolveWith) => {
+      this.waitFecthingInterval = setInterval(async () => {
+        clearInterval(this.waitFecthingInterval);
+        if (this.fetching) {
+          resolveWith(this.waitForPendingFetching());
+        } else {
+          resolveWith(true);
+        }
+      }, 1000);
+    });
   };
 
   queueShiftFecthAppendBuffer = async (isFirst = false) => {
@@ -125,13 +133,15 @@ class QueueMediaPlayer {
       this.lastOffset += isFirst ? 0 : videoFecth.duration;
       this.sourceBuffer.timestampOffset = this.lastOffset;
       this.sourceBuffer.appendBuffer(videoFecth.buffer);
-      this.totalDuration += videoFecth.duration;
+      this.totalBufferDuration += videoFecth.duration;
       this.queueWaiting = this.queue.length == 0;
+
       this.timeRanges.push({
         url: url,
         from: this.lastOffset,
         to: this.lastOffset + videoFecth.duration,
         offset: this.lastOffset,
+        duration: videoFecth.duration,
       });
 
       // callback event
@@ -159,8 +169,8 @@ class QueueMediaPlayer {
       } catch (error) {
         console.log('There was a network error.');
         this.networkWaiting = true;
-        this.fecthRetryInterval = setInterval(async () => {
-          clearInterval(this.fecthRetryInterval);
+        this.fetchingRetryInterval = setInterval(async () => {
+          clearInterval(this.fetchingRetryInterval);
           const resp = await this.fecthForever(vidUrl);
           resolveWith(resp);
         }, 5000);
